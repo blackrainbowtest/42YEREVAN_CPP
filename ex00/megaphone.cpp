2e5f0a2e35aa59ab0adf8ca73e3d4378e11eae7b
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-02-03 17:23:02 +0400

    refactor: update

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,9 +2,18 @@
 
 int	main(int argc, int argv)
 {
+	int	i;
+	int	j;
+
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-
+	i = -1;
+	while (argc > 1 && argv[++i])
+	{
+		j = -1;
+		while (argv[i] && argv[i][++j])
+			std::cout << (char)std::toupper(argv[i][j]);
+	}
 	std::cout << std::endl;
 	return (0);
 }
