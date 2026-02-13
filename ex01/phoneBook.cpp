2023f1b253517e7f4be794870a72307cd3303a64
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-02-13 10:44:37 +0400

    refactor: updated

---
+++ ex01/phoneBook.cpp
@@ -0,0 +1,1 @@
+#include "phoneBook.hpp"
--- ex01/phoneBook.hpp
+++ ex01/phoneBook.hpp
@@ -3,4 +3,18 @@
 
 # include <string>
 
+class PhoneBook
+{
+	private:
+		Contact	_contacts[8];
+		int		_index;
+		bool	_isFull;
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+
+		void	set_information(void);
+		void	get_information(void)const;
+		void	get_instruction(void);
+};
 #endif // ! PHONEBOOK_HPP
