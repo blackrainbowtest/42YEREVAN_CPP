4283af702e0326ec53350df5e3d0c57980249faa
blackrainbowtest <araqelyan.aram.96@gmail.com>
2026-02-15 21:09:17 +0400

    фвв

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,9 +10,18 @@ int	main(void)
 	PhoneBook.getInstruction();
 	while (!isExit && std::getline(std::cin, input))
 	{
-
+		if (input.compare("ADD") == 0)
+			phoneBook.addContact();
+		else if (input.compare("SEARCH") == 0)
+			phoneBook.searchContact();
+		else if (input.compare("EXIT") == 0)
+		{
+			std::cout << "Exiting programm." << std::endl;
+			isExit = true;
+		}
+		phoneBook.getInstruction();
 	}
 	if (!isExit)
-		std::cout << "Exiting programm." << std::endl;
+		std::cout << "You Pressed ^D. Exiting phonebook now." << std::endl;
 	return (0);
 }
