84ba68debbe0f3998cc4096ad2634478cce6eaaa
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-02-25 11:38:12 +0400

    refactor: updated

---
+++ ex02/Account.cpp
@@ -0,0 +1,10 @@
+#include "Account.hpp"
+#include <iostream>
+
+Account::_nbAccounts = 0;
+Account::_totalAmount = 0;
+Account::_totalNbDeposits = 0;
+Account::_totalNbWithdrawals = 0;
+
+
+// Account.cpp
