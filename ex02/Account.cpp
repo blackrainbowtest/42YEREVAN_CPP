d7cb06b2230038568f13c94fe4a3c83838269d5c
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-02-25 15:28:20 +0400

    refactor

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,10 +1,26 @@
 #include "Account.hpp"
 #include <iostream>
 
-Account::_nbAccounts = 0;
-Account::_totalAmount = 0;
-Account::_totalNbDeposits = 0;
-Account::_totalNbWithdrawals = 0;
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
+Account::Account() {}
 
+Account::Account( int initial_deposit ) {
+	this->_accountIndex = Account::_nbAccounts;
+	Account::_nbAccounts++;
+	this->_amount = initial_deposit;
+	Account::_totalAmount += this->_amount;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_displayTimestamp();
+	std::cout << "index:" << "sone arg here"
+}
+
+Account::~Account( void ) {
+
+}
 // Account.cpp
