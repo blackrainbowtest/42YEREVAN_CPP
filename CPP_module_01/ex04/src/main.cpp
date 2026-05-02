63250e792e99a41f7db23b23f972bcb9d542024b
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2026-05-02 15:27:21 +0400

    updated

---
+++ CPP_module_01/ex04/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 15:22:41 by aramarak          #+#    #+#             */
+/*   Updated: 2026/05/02 15:26:35 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define BLACK "\033[30m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define BG_MAGENTA "\033[45m"
+#define BG_CYAN "\033[46m"
+#define RESET "\033[0m"
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 4)
+	{
+		std::cout << MAGENTA << "Wrong number of args"
+				<< RESET << std::endl;
+	}
+	return (EXIT_SUCCESS);
+}
