be915653efef8c90e57a525b8ea7cb0da760af70
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2026-05-08 20:55:01 +0400

    add

--- CPP_module_04/ex00/src/Cat.cpp
+++ CPP_module_04/ex00/src/Cat.cpp
@@ -1,12 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 20:07:22 by aramarak          #+#    #+#             */
-/*   Updated: 2026/05/08 20:07:23 by aramarak         ###   ########.fr       */
+/*   Updated: 2026/05/08 20:37:45 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Cat.hpp"
+
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define BLACK		"\033[30m"
+#define RED			"\033[31m"
+#define YELLOW		"\033[33m"
+#define MAGENTA		"\033[35m"
+#define CYAN		"\033[36m"
+
+#define BG_RED		"\033[41m"
+#define BG_YELLOW	"\033[43m"
+#define BG_MAGENTA	"\033[45m"
+#define BG_CYAN		"\033[46m"
+
+#define RESET		"\033[0m"
+
+Cat::Cat(): Animal()
+{
+	
+}
