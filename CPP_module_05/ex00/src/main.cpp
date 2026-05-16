71a3a383f30fa418311fec59130d6ea9a4251816
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2026-05-16 10:39:25 +0400

    05 add

---
+++ CPP_module_05/ex00/src/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 16:54:04 by aramarak          #+#    #+#             */
+/*   Updated: 2026/05/16 10:38:59 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define BLACK		"\033[30m"
+#define BLUE		"\033[34m"
+#define RED			"\033[31m"
+#define YELLOW		"\033[33m"
+#define MAGENTA		"\033[35m"
+#define CYAN		"\033[36m"
+
+#define BG_RED		"\033[41m"
+#define BG_YELLOW	"\033[43m"
+#define BG_MAGENTA	"\033[45m"
+#define BG_CYAN		"\033[46m"
+
+#define RESET		"\033[0m"
+
+static void	printDivider(void)
+{
+	std::cout << std::endl;
+	std::cout << "-------------------------------------------------------" << std::endl;
+	std::cout << std::endl;
+}
+
+static void	printSection(const std::string& title)
+{
+	printDivider();
+	std::cout << BLUE << title << RESET << std::endl;
+	printDivider();
+}
+
+int	main(void)
+{
+	printSection("Test");
+	
+	
+	return (EXIT_SUCCESS);
+}
