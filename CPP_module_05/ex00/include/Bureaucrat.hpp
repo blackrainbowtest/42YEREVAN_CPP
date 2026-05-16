9d836d3321febb881b4e5ac636e3c73d6f80bf23
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2026-05-16 11:52:07 +0400

    add

---
+++ CPP_module_05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/16 10:51:26 by aramarak          #+#    #+#             */
+/*   Updated: 2026/05/16 11:05:43 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+	
+}
+
+std::ostream& operator<<(std::ostream& o, Bureaucrat* a);
+
+#endif
