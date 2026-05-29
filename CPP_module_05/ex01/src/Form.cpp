d51b7d077f883c22ccd0058c7f28428d3c5406d0
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-05-29 11:32:13 +0400

    add

--- CPP_module_05/ex01/src/Form.cpp
+++ CPP_module_05/ex01/src/Form.cpp
@@ -20,3 +20,88 @@ Form::Form(void): _name("default"), _is_signed(false), _sign_req_grade(150),
 	std::cout << "Form Default Constructor called" << std::endl;
 }
 
+Form::Form(const std::string name, const int sign_req_grade, const int exec_req_grade)
+        : _name(name), _is_signed(false), _sign_req_grade(sign_req_grade),
+        _exec_req_grade(exec_req_grade)
+{
+    validateGrade(sign_req_grade);
+    validateGrade(exec_req_grade);
+    std::cout << GREEN << "Form constructor with sign and execution grade called" << RESET << std::endl;
+}
+
+Form::Form(Form const & src): _name(src._name), _is_signed(src._is_signed),
+        _sign_req_grade(src._sign_req_grade), _exec_req_grade(src._exec_req_grade)
+{
+	std::cout << GREEN << "Form copy constructor called" << RESET << std::endl;
+}
+
+Form::~Form()
+{
+	std::cout << RED << "Form destructor called" << RESET << std::endl;
+}
+
+void Form::validateGrade(int grade)
+{
+    if (grade < 1)
+		throw Form::GradeTooHighException();
+	if (grade > 150)
+		throw Form::GradeTooLowException();
+}
+
+Form &Form::operator=(const Form &src)
+{
+    std::cout << GREEN << "Form assignment operator called" << RESET << std::endl;
+	if (this != &src)
+		this->_is_signed = src._is_signed;
+
+	return (*this);
+}
+
+const std::string&  Form::getName(void) const
+{
+    return (this->_name);
+}
+
+bool    Form::getIsSigned(void) const
+{
+    return (this->_is_signed);
+}
+int     Form::getSignGrade(void)const
+{
+    return (this->_sign_req_grade);
+}
+int     Form::getExecGrade(void)const
+{
+    return (this->_exec_req_grade);
+}
+
+void Form::beSigned(const Bureaucrat &signer)
+{
+    if (this->_sign_req_grade < signer.getSignGrade())
+        throw Form::GradeTooLowException();
+    if (this->getIsSigned())
+    {
+        std::cout << BLUE << "Form is already signed" << RESET << std::endl;
+    }
+    else
+    {
+        this->_is_signed = true;
+        std::cout << GREEN << "Form was successfully signed" << RESET << std::endl;
+    }
+}
+
+const char *Form::GradeTooLowException::what(void) const throw()
+{
+    return ("Grade too low");
+}
+
+const char *Form::GradeTooHighException::what(void) const throw()
+{
+    return ("Grade too high");
+}
+
+std::ostream & operator<<(std::ostream &os, Form const & src)
+{
+    // add out here
+    return (os);
+}
