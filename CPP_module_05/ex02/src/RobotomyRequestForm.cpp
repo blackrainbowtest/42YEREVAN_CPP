fe61aaa37c8ab1035959e9b4e25760be8ae5e4c7
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-06-04 15:39:44 +0400

    add

--- CPP_module_05/ex02/src/RobotomyRequestForm.cpp
+++ CPP_module_05/ex02/src/RobotomyRequestForm.cpp
@@ -13,4 +13,75 @@
 #include "color_palletre.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "Bureaucrat.hpp"
-# include <cstdlib>
+#include <cstdlib>
+
+RobotomyRequestForm::RobotomyRequestForm(void)
+    : AForm("RobotomyRequestForm", 72, 45), _target("default")
+{
+    std::cout << GREEN << "RobotomyRequestForm Default Constructor called" << RESET << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target)
+    : AForm("RobotomyRequestForm", 72, 45), _target(target)
+{
+    std::cout << GREEN << "RobotomyRequestForm Constructor for "
+        << YELLOW << this->getTarget() << GREEN << " called" 
+        << RESET << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &src)
+    : AForm(src), _target(src.getTarget())
+{
+    std::cout << GREEN << "RobotomyRequestForm copy constructor called"
+		<< RESET << std::endl;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()
+{
+	std::cout << RED << "RobotomyRequestForm destructor called"
+		<< RESET << std::endl;
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &src)
+{
+	if (this != &src)
+	{
+		AForm::operator=(src);
+		this->_target = src._target;
+	}
+	return (*this);
+}
+
+void RobotomyRequestForm::execute(Bureaucrat const &executor) const
+{
+	validateExecution(executor);
+
+    std::cout << "* DRRRRRRRRRRRRRR *" << std::endl;
+
+	if (std::rand() % 2)
+	{
+		std::cout << this->_target
+			<< " has been robotomized successfully."
+			<< std::endl;
+	}
+	else
+	{
+		std::cout << "Robotomy failed on "
+			<< this->_target
+			<< "."
+			<< std::endl;
+	}
+}
+
+const std::string &RobotomyRequestForm::getTarget(void) const
+{
+	return (this->_target);
+}
+
+std::ostream &operator<<(std::ostream &os, const RobotomyRequestForm &src)
+{
+	os << src.getName()
+		<< " target: "
+		<< src.getTarget();
+	return (os);
+}
