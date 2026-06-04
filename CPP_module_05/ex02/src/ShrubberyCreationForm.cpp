5cf54b5d2207893aef1cdfdfb5a330cf7b580cec
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-06-04 16:36:36 +0400

    add

--- CPP_module_05/ex02/src/ShrubberyCreationForm.cpp
+++ CPP_module_05/ex02/src/ShrubberyCreationForm.cpp
@@ -10,4 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "color_palletre.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "Bureaucrat.hpp"
 
+ShrubberyCreationForm::ShrubberyCreationForm(void)
+    : AForm("ShrubberyCreationForm", 72, 45), _target("default")
+{
+    std::cout << GREEN << "ShrubberyCreationForm Default Constructor called" << RESET << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target)
+    : AForm("ShrubberyCreationForm", 72, 45), _target(target)
+{
+    std::cout << GREEN << "ShrubberyCreationForm Constructor for "
+        << YELLOW << this->getTarget() << GREEN << " called" 
+        << RESET << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &src)
+    : AForm(src), _target(src.getTarget())
+{
+    std::cout << GREEN << "ShrubberyCreationForm copy constructor called"
+		<< RESET << std::endl;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+	std::cout << RED << "ShrubberyCreationForm destructor called"
+		<< RESET << std::endl;
+}
