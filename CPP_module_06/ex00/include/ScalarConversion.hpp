791c839d1686fbe611b45d311b9442d9e3ca2dcc
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-06-09 11:41:39 +0400

    09.06.26 11:41

---
+++ CPP_module_06/ex00/include/ScalarConversion.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConversion.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/16 10:51:26 by aramarak          #+#    #+#             */
+/*   Updated: 2026/05/16 11:05:43 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERSION_HPP
+# define SCALARCONVERSION_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+# include <limits>
+# include <climits>
+# include <cstdlib>
+# include <cstring>
+# include <cctype>
+
+class ScalarConverter
+{
+    private:
+        enum e_type
+        {
+            ERROR = 0, // unknown input type
+            CHAR, // char ubput type
+            INT, // integer input type
+            FLOAT, // float input type
+            DOUBLE, // double input type
+            NAN_INF // NaN or infinity types
+        };
+    public:
+
+        ScalarConverter &operator=(const ScalarConverter &src);
+
+    class ErrorException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw();
+    };
+};
+
+#endif // !CONVERSION_HPP
