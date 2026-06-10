4d157e359670b2a2c9d5a0099952129ccb36e663
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-06-10 11:47:14 +0400

    26/06/10 11:47

--- CPP_module_06/ex00/include/ScalarConverter.hpp
+++ CPP_module_06/ex00/include/ScalarConverter.hpp
@@ -28,21 +28,37 @@ class ScalarConverter
         enum e_type
         {
             ERROR = 0, // unknown input type
-            CHAR, // char input type
-            INT, // integer input type
-            FLOAT, // float input type
-            DOUBLE, // double input type
-            NAN_INF // NaN or infinity types
+            CHAR,      // char input type
+            INT,       // integer input type
+            FLOAT,     // float input type
+            DOUBLE,    // double input type
+            NAN_INF    // NaN or infinity types
         };
-    public:
 
+        // Constructors
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter &src);
+        // Destructors
+        ~ScalarConverter();
+        // Overloaded Operators
         ScalarConverter &operator=(const ScalarConverter &src);
+        // Methods
+		static e_type detectType(const std::string &literal);
 
-    class ErrorException : public std::exception
-    {
-        public:
-            virtual const char *what() const throw();
-    };
+        static bool isChar(const std::string &literal);
+        static bool isInt(const std::string &literal);
+        static bool isFloat(const std::string &literal);
+        static bool isDouble(const std::string &literal);
+        static bool isPseudoLiteral(const std::string &literal);
+    public:
+        // Methods
+        static void convert(const std::string &literal);
+        //Exceptions
+        class ErrorException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
 };
 
 #endif // !SCALARCONVERTER_HPP
