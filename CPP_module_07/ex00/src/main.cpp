c7cc2833d9407e7e5b4ed60842cde1b9ef0e4f55
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-08-03 17:30:23 +0400

    add

--- CPP_module_07/ex00/src/main.cpp
+++ CPP_module_07/ex00/src/main.cpp
@@ -9,3 +9,31 @@
 /*   Updated: 2026/08/03 16:16:41 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int main()
+{
+    // int
+    {
+        int a = 5;
+        int b = 6;
+
+
+    }
+    // float
+    {
+        float a = 5.4f;
+        float b = 3.7f;
+
+
+    }
+    // string
+    {
+        std::string a = "string a";
+        std::string b = "string b";
+        
+    }
+    return (0);
+}
