ae6cdafab43f2815e70d07f1728c1008dab0c617
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-08-13 17:07:31 +0400

    add

--- CPP_module_08/ex00/include/easyfind.hpp
+++ CPP_module_08/ex00/include/easyfind.hpp
@@ -5,27 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/23 09:50:45 by aramarak          #+#    #+#             */
-/*   Updated: 2026/07/23 10:57:10 by aramarak         ###   ########.fr       */
+/*   Created: 2026/08/12 14:42:04 by aramarak          #+#    #+#             */
+/*   Updated: 2026/08/13 17:05:42 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-template < typename T > // can define like template < class T > // too
+# include <algorithm>
+# include <iterator>
+# include <iostream>
+# include <exception>
+
+class NotFoundException: public std::exception
+{
+    public:
+        virtual const char *what() const throw()
+        {
+            return ("Not found");
+        }
+}
+
+template < typename T> // can define like template < class T > // too
 const T &max(const T &x, const T &y)
 {
     return (x > y ? x : y);
 }
 
-template < typename T >
+template < typename T>
 const T &min(const T &x, const T &y)
 {
     return (x < y ? x : y);
 }
 
-template < typename T >
+template < typename T>
 void    swap(T &x, T &y)
 {
     T temp = x;
