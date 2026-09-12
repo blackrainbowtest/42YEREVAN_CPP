85d09192d08a39461bac502111fb9e9a547a8fe6
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2026-09-12 18:27:57 +0400

    updated ex02

--- CPP_module_06/ex02/src/main.cpp
+++ CPP_module_06/ex02/src/main.cpp
@@ -17,6 +17,7 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include "color_palletre.hpp"
 
 static Base *generate(void)
 {
@@ -41,28 +42,47 @@ static void identify(Base *Test)
 {
 	if (dynamic_cast<A *>(Test))
 	{
-		std::cout << "identified type is A" << std::endl;
+		std::cout << CYAN << "pointer" << RESET << " identified type is A" << std::endl;
 	}
 	else if (dynamic_cast<B *>(Test))
 	{
-		std::cout << "identified type is B" << std::endl;
+		std::cout << CYAN << "pointer" << RESET << " identified type is B" << std::endl;
 	}
-	else if (dynamic_cast<C *>(test))
+	else if (dynamic_cast<C *>(Test))
 	{
-		std::cout << "identified type is C" << std::endl;
+		std::cout << CYAN << "pointer" << RESET << " identified type is C" << std::endl;
 	}
 	else
 		std::cout << "unknown type" << std::endl;
 }
 
-static int i = 0;
-static std::string classes[] = {"A", "B", "C"};
-
 static void identify(Base &Test)
 {
-	
-	
-	//TODO: Use dynamic_cast to identify the type of the object
+	try
+	{
+		(void)dynamic_cast<A &>(Test);
+		std::cout << MAGENTA << "reference" << RESET << " identified type is A" << std::endl;
+		return ;
+	}
+	catch(...)
+	{}
+	try
+	{
+		(void)dynamic_cast<B &>(Test);
+		std::cout << MAGENTA << "reference" << RESET << " identified type is B" << std::endl;
+		return ;
+	}
+	catch(...)
+	{}
+	try
+	{
+		(void)dynamic_cast<C &>(Test);
+		std::cout << MAGENTA << "reference" << RESET << " identified type is C" << std::endl;
+		return ;
+	}
+	catch(...)
+	{}
+	std::cout << "unknown type" << std::endl;
 }
 
 
