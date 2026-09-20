e1c3a9f6b01a17a1adbf61dd10ee91f5df262ae3
splitshard <araqelyan.aram.96@gmail.com>
2026-09-20 20:54:56 +0400

    add

---
+++ CPP_module_08/ex01/include/Span.hpp
@@ -0,0 +1,17 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <exception>
+#include <vector>
+#include <iterator>
+#include <climits>
+
+class Span
+{
+
+}
+
+#endif // SPAN
