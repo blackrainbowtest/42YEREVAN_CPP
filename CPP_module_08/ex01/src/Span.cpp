7e915d42c406c90320c7f9ed15d415234b32db0b
splitshard <araqelyan.aram.96@gmail.com>
2026-09-21 19:58:01 +0400

    фвв

--- CPP_module_08/ex01/src/Span.cpp
+++ CPP_module_08/ex01/src/Span.cpp
@@ -1,13 +1,52 @@
 #include "Span.hpp"
 
 // Constructors
+Span::Span(unsigned int N) : _size(N) 
+{
+	std::cout << "Span Constructor for size of " << N << " called" << std::endl;
+	this->_storage.reserve(this->_size);
+}
+
+Span::Span(const Span &src) : _storage(src._storage), _size(src._size) 
+{
+	std::cout << "Span Copy Constructor called" << std::endl;
+	// *this = src; // unnecessary: members are already copied by the initializer list
+}
 
 // Destructor
+Span::~Span() {
+	std::cout << "Span Deconstructor called" << std::endl;
+}
 
 // Overloaded Operators
+Span	&Span::operator=(const Span &src)
+{
+	std::cout << "Span Assignation operator called" << std::endl;
+
+	if (this == &src)
+		return *this;
+
+	this->_storage = src._storage;
+	this->_size = src._size;
+
+	return *this;
+}
 
 // Public Methods
 
 // Getter
+unsigned int	Span::getSize() const
+{
+	return (this->_size);
+}
 
 // Exceptions
+const char	*Span::ArrayFullException::what() const throw()
+{
+	return ("Error: Span is full");
+}
+
+const char	*Span::ComparisonInvalidException::what() const throw()
+{
+	return ("Error: at least two numbers are required");
+}
