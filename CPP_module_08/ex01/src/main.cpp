3a77479f9d9d3a38495d482cdf419b7e31ac9cad
splitshard <araqelyan.aram.96@gmail.com>
2026-09-22 23:19:21 +0400

    add

--- CPP_module_08/ex01/src/main.cpp
+++ CPP_module_08/ex01/src/main.cpp
@@ -2,28 +2,13 @@
 
 int main()
 {
-	{
-		Span a = Span(5);
-		for (size_t i = 0; i < 6; i++)
-		{
-			try
-			{
-				a.addNumber(i);
-			}
-			catch (std::exception &e)
-			{
-				std::cerr << std::endl << e.what() << std::endl;
-			}
-		}
-		std::cout << "shortest span is " << a.shortestSpan() << std::endl;
-		std::cout << "longest span is " << a.longestSpan() << std::endl;
-	}
-	std::cout << std::endl << "---------------------------------------------------------------" << std::endl << std::endl;
-	{
-		Span a = Span(10000);
-		a.addNumber(10000, time(NULL));
-		std::cout << "shortest span is " << a.shortestSpan() << std::endl;
-		std::cout << "longest span is " << a.longestSpan() << std::endl << std::endl;
-	}
-	return (0);
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
 }
