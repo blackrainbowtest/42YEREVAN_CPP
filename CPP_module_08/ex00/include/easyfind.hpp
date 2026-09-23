0fea352f5f1b284fc3f0a76de6ea6fc5272eda38
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2026-09-23 19:00:22 +0400

    add

--- CPP_module_08/ex00/include/easyfind.hpp
+++ CPP_module_08/ex00/include/easyfind.hpp
@@ -6,6 +6,21 @@
 # include <iostream>
 # include <exception>
 
+# define BLACK		"\033[30m"
+# define GREEN		"\033[32m"
+# define BLUE		"\033[34m"
+# define RED		"\033[31m"
+# define YELLOW		"\033[33m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+
+# define BG_RED		"\033[41m"
+# define BG_YELLOW	"\033[43m"
+# define BG_MAGENTA	"\033[45m"
+# define BG_CYAN	"\033[46m"
+
+#define RESET		"\033[0m"
+
 class NotFoundException: public std::exception
 {
     public:
@@ -13,7 +28,7 @@ class NotFoundException: public std::exception
         {
             return ("Not found");
         }
-}
+};
 
 template <typename T>
 typename T::iterator easyfind(T &container, int value)
--- CPP_module_08/ex00/src/main.cpp
+++ CPP_module_08/ex00/src/main.cpp
@@ -8,10 +8,13 @@ int main()
 	test.push_back(669);
 	test.push_back(670);
 
+	std::cout << GREEN << "Pushed three numbers: 668, 669, 670" << RESET << std::endl;
+
 	std::list<int>::const_iterator	it = test.end();
 
 	try
 	{
+		std::cout << BLUE << "Try find 668:\t" << RESET;
 		it = ::easyfind(test, 668);
 	}
 	catch(const std::exception& e)
@@ -26,6 +29,7 @@ int main()
 
 	try
 	{
+		std::cout << RED << "Try find 111:\t" << RESET;
 		it = ::easyfind(test, 111);
 	}
 	catch(const std::exception& e)
